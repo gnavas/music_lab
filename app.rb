c5238a1755d76412c953494fe13a345c2980519a
George Navas <gannavas@gmail.com>
2015-02-18 20:08:52 -0800

    hw first version

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 require 'pry'
 require "sinatra/activerecord"
 require 'pg'
+require './models/artist'
 
 
 configure :development do
@@ -11,6 +12,45 @@ configure :development do
   BetterErrors.application_root = __dir__
 end
 
+#redirect to artists
 get '/' do
-"Hello World"
+redirect '/artists'
 end
+#reads all artists
+get '/artists' do
+ @artists = Artists.all
+  erb :index
+end
+# render a form to create a new artists
+get '/artists/new' do
+  erb :new
+end
+# - reads a single artists
+get '/artists/:id' do
+@artist = Artists.find_by(id: params[:id])
+erb :show
+end
+# render a form to edit an artist
+get '/artists/:id/edit' do
+  @artist = Artists.find_by(id: params[:id])
+  erb :edit
+end
+
+post '/artists/new' do
+  Artists.create(name: params[:name]) 
+  redirect '/'
+end
+
+put '/artists/:id' do
+  artist = Artists.find_by(id: params[:id])
+  artist.update(name: params[:name])
+  redirect '/artists'
+  end
+
+ delete '/artists/:id/delete' do
+  artist = Artists.find_by(id: params[:id])
+  artist.destroy
+  redirect '/artists'
+  end 
+
+
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Artists.destroy_all
+Artists.create({:name => "The Beatles"})
+Artists.create({:name => "Blink 182"})
